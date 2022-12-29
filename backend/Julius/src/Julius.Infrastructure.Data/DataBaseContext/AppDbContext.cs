c408cb37a554016267cb1ae2d9e8348919da71e4
DaniFTT <daniel.s.6140@gmail.com>
2022-12-28 22:53:00 -0300

    Criado AppDbContext e EfRepository

---
+++ backend/Julius/src/Julius.Infrastructure.Data/DataBaseContext/AppDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Julius.SharedKernel.Interfaces;
+using Julius.SharedKernel;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Julius.Domain.CategoryAggregate;
+
+namespace Julius.Infrastructure.Data.DataBaseContext;
+
+public class AppDbContext : DbContext
+{
+    private readonly IDomainEventDispatcher? _dispatcher;
+
+    public AppDbContext(DbContextOptions<AppDbContext> options,
+        IDomainEventDispatcher? dispatcher)
+        : base(options)
+    {
+        _dispatcher = dispatcher;
+    }
+
+    public DbSet<Category> Categories => Set<Category>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        int result = await base.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+
+        if (_dispatcher == null) return result;
+
+        var entitiesWithEvents = ChangeTracker.Entries<EntityBase>()
+            .Select(e => e.Entity)
+            .Where(e => e.DomainEvents.Any())
+            .ToArray();
+
+        await _dispatcher.DispatchAndClearEvents(entitiesWithEvents);
+
+        return result;
+    }
+
+    public override int SaveChanges()
+    {
+        return SaveChangesAsync().GetAwaiter().GetResult();
+    }
+}
---
+++ backend/Julius/src/Julius.Infrastructure.Data/Repositories/EfRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification.EntityFrameworkCore;
+using Julius.Infrastructure.Data.DataBaseContext;
+using Julius.SharedKernel.Interfaces;
+
+namespace Julius.Infrastructure.Data.Repositories;
+
+public class EfRepository<T> : RepositoryBase<T>, IReadRepository<T>, IRepository<T> where T : class, IAggregateRoot
+{
+    public EfRepository(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+}
+
