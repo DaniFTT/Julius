f14e645c6c4a15a382e1933fbd97ae594dd84cf5
DaniFTT <daniel.s.6140@gmail.com>
2022-12-28 22:54:08 -0300

    [WIP] Criada classe de commando para criação de categoria

---
+++ backend/Julius/src/Julius.Application/Categories/Commands/CreateCategory/CreateCategoryCommand.cs
@@ -0,0 +1,15 @@
+﻿using Julius.Domain.CategoryAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Julius.Application.Categories.Commands.CreateCategory
+{
+    public class CreateCategoryCommand
+    {
+        public string Name { get; set; }
+        public CategoryType Type { get; set; }
+    }
+}
---
+++ backend/Julius/src/Julius.Application/Categories/Commands/CreateCategory/CreateCategoryCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using Julius.Domain.CategoryAggregate;
+
+namespace Julius.Application.Categories.Commands.CreateCategory
+{
+    public class CreateCategoryCommandHandler
+    {
+        public CreateCategoryCommandHandler()
+        {
+
+        }
+
+        public Category Handle(string name, CategoryType type)
+        {
+            return new Category(name, type, "12345");
+        }
+    }
+}
