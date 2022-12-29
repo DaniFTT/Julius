43821007ab450ec21c14f7b4879622a331e1eacd
DaniFTT <daniel.s.6140@gmail.com>
2022-12-28 22:52:30 -0300

    Criada pasta de CategoryAggregate
    Nela foi incluido o AggregateRoot de Category, e seu Enum CategoryType

---
+++ backend/Julius/src/Julius.Domain/CategoryAggregate/Category.cs
@@ -0,0 +1,21 @@
+﻿using Julius.SharedKernel;
+using Julius.SharedKernel.Interfaces;
+
+namespace Julius.Domain.CategoryAggregate
+{
+    public class Category : EntityBase, IAggregateRoot
+    {
+        public string? Name { get; set; }
+        public CategoryType? Type { get; set; }
+        public string? UserId { get; set; }
+
+        public Category() : base() { }
+
+        public Category(string name, CategoryType type, string? userId) : base()
+        {
+            Name = name;
+            Type = type;
+            UserId = userId;
+        }
+    }
+}
---
+++ backend/Julius/src/Julius.Domain/CategoryAggregate/CategoryType.cs
@@ -0,0 +1,13 @@
+﻿
+using Ardalis.SmartEnum;
+
+namespace Julius.Domain.CategoryAggregate
+{
+    public class CategoryType : SmartEnum<CategoryType>
+    {
+        public static readonly CategoryType Expense = new(nameof(Expense), 0);
+        public static readonly CategoryType Income = new(nameof(Income), 1);
+
+        protected CategoryType(string name, int value) : base(name, value) { }
+    }
+}
