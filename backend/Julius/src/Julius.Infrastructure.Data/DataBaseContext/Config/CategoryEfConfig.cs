4eaf22562f02441cdf6c2a37298b6164888fcaf6
DaniFTT <daniel.s.6140@gmail.com>
2022-12-28 22:53:30 -0300

    Criada classe de configuração para Entidade Category para a EF Migration

---
+++ backend/Julius/src/Julius.Infrastructure.Data/DataBaseContext/Config/CategoryEfConfig.cs
@@ -0,0 +1,30 @@
+﻿using Julius.Domain.CategoryAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Julius.Infrastructure.Data.DataBaseContext.Config;
+
+public class CategoryEFConfig : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.Property(p => p.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(p => p.Type)
+            .IsRequired()
+            .HasConversion(
+                p => p.Value,
+                p => CategoryType.FromValue(p));
+
+        builder.Property(p => p.UserId)
+            .IsRequired();
+    }
+}
+
